3e143c95cf06e9c335c2c7c9c3c8efb832730328
rachelnoireau <rachel.noireau@uha.fr>
2017-05-30 14:15:23 +0200

    nouv fichier

--- inscription.php
+++ inscription.php
@@ -117,41 +117,6 @@
 					</label> 
 				</div>
 			</form>
-            
-            <?php
-            
-      
-             if(isset($_POST['envoi'])){ // si le bouton envoi a été cliqué
-        if(!empty($_POST['nom'])){ // si le champ nom a été rempli
-            $nom = $_POST['nom']; // stocker la valeur qu'il contient dans une variable
-            if(!empty($_POST['prenom'])){ // si le champ prenom a été rempli
-                $prenom = $_POST['prenom']; // stocker la valeur qu'il contient dans une variable
-                if(!empty($_POST['adresse'])){ 
-                    $adresse = $_POST['adresse']; 
-                    if(!empty($_POST['mail'])){ // si le champ mail a été rempli
-                        $mail = $_POST['mail']; // stocker la valeur qu'il contient dans une variable
-                        if(!empty($_POST['date de naissance'])){ 
-                            $date = $_POST['date de naissance']; 
-                            if(!empty($_POST['promo'])){ 
-                                $promo = $_POST['promo']; 
-                                if(!empty($_POST['filiere'])){ 
-                                    $filiere = $_POST['filiere']; 
-                                    
-                                 
-                                $insertion = $bdd->prepare('INSERT INTO profil VALUES("'.$nom.'","'.$prenom.'","'.$adresse.'","'.$mail.'","'.$date.'","'.$promo.'","'.$filiere.'","'.$date.'")'); // préparation de la requête d'insertion dans la base de données
-                                $insertion->execute();  // exécution de l'insertion
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-                                
-                                
-                                
-            ?>
     
  <?php
    
