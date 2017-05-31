8acdeb4ca566c3d96754ee1b0a92cb163d27758d
NicolasMG <nicolas.m.greiner@gmail.com>
2017-05-31 15:51:44 +0200

    Notification
    Brèves modifications + commentaire des besoins sur cette page

--- connexion.php
+++ connexion.php
@@ -1,15 +1,27 @@
 <?php
 
-//verification de l'adress mail
+//verification de l'adress mail :
 
 
 
-//verification du mot de passe ici
-    session_start();
-    $_SESSION['ID']=$_POST['ID'];
-   // <a href="./profil.php">mon compte</a>
+//verification du mot de passe ici :
+$motDePass = /*  */;
+$sel = /*  */;
+$testMotDePass = password_verify($motDePass, $sel);
+if($testMotDePass == 0) {
+    // Mauvais mot de passe
+    //renvoyer sur page de connexion avec message d'erreur;
+}
+else {
 
+        session_start();
+        $_SESSION['ID']=$_POST['ID'];
+       // <a href="./profil.php">mon compte</a>
+
+}   // pas sur si on doit d'arreter ici ou plus loin (doute de mon niveau de php)
 ?>
-    <!DOCTYPE html>
-   <a href="./profil.php">mon compte</a>
-<!--peut etre inserer allieur pour pas avoir de page en plus
+        <!DOCTYPE html>
+       <a href="./profil.php">mon compte</a>
+    <!--peut etre inserer ailleur/autrement pour pas avoir de page en plus
+
+
