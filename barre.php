b2663cd375e6a785ab0bf55e1d0844588dfaa686
rachelnoireau <rachel.noireau@uha.fr>
2017-06-01 12:11:40 +0200

    barre

---
+++ barre.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+	<header>
+	
+
+
+
+<ul  id="menu_horizontal">
+	<li class="bouton_gauche">
+		<img src="stork-talk_01.png" alt="Photo de montagne" width="55" />
+	</li>
+	
+	<li class="bouton_gauche1">
+		<form>
+			<input type="text" name="Search" placeholder="Recherche..">
+		</form>
+	</li>
+	
+	
+	<li class="bouton_droite">
+		
+			<a href="header.html" ><img src="roue.png" alt="Photo de montagne" width="35"  /></a>
+		
+	</li>
+	
+	
+	<li class="bouton_droite">
+		
+		<img src="ami.png" alt="Photo de montagne" width="35"  />
+		
+	</li>
+	
+	<li class="bouton_droite">
+		
+			<img src="enveloppe.png" alt="Photo de montagne" width="35"  />
+		
+	</li>
+	
+	<li class="bouton_droite">
+		
+			<img src="alarme.png" alt="Photo de montagne" width="35"   />
+			
+		
+	</li>
+	
+	
+	
+	
+</ul>
+
+
+
+
+	</header>
+	
+</body>
+</html>
