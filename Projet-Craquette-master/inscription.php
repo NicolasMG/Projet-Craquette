2580b8c6cf0d1421322a6d80ed045943171705d1
rachelnoireau <rachel.noireau@uha.fr>
2017-06-01 11:36:36 +0200

    bla

---
+++ Projet-Craquette-master/inscription.php
@@ -0,0 +1,110 @@
+<?php
+    try{ 
+        $bdd = new PDO('mysql:host=localhost;dbname=siteweb;charset=utf8','root',''); // stocker la connexion à la base de données dans la variable $bdd
+    }
+    catch(Exception $e){ // si cela ne fonctionne pas : attraper l'erreur...
+        die('Erreur : '.$e->getMessage()); // ... arrêter le processus et afficher l'erreur
+    }
+	
+    include('header.php');
+	
+    // $reponse2 = $bdd->query('SELECT * FROM profil ORDER BY id DESC'); 
+?>
+
+		
+		<section> <!-- Contenu principal de la page -->
+	
+            <h2>Inscription</h2>
+                
+            <form class="form-horizontal" method="post" action="traitement.php" >
+             
+                <div class="form-group">
+					<label for="nom">
+						Nom:
+					</label>
+					<input type="text" class="input-medium search-query" name="nom" placeholder= "Votre nom" />  
+				</div>
+                
+                <div>
+					<label for="prenom">
+						Prénom:
+					</label>
+					<input type="text" class="input-medium search-query" name="prenom" placeholder= "Votre prénom" />
+                </div>
+                
+                <div>
+					<label for="mail">
+						Mail:
+					</label>
+					<input type="email" class="input-medium search-query" class="form-control" name="mail" placeholder= "Votre email" />
+				</div>
+                
+          
+				<p>
+                <label for="filiere">
+						Filiere:
+					</label>
+				<select name="filiere">
+					<option value="IR"> IR </option>
+					<option value="AS"> AS </option>
+                    <option value="TF"> TF</option>
+                    <option value="Méca"> Méca </option>
+                    <option vallue="Fip"> FIP</option>
+                    <option value="enseignant"> enseignant</option>
+                    <option value="autre"> autre</option>    
+				</select>
+                </p>
+                <p>
+                    <label for="promo">
+						Année d'etude:
+					</label>
+                    <select name="promo">
+                        <option value="1A">1A</option>
+                        <option value="2A">2A</option>
+                        <option value="3A">3A</option>
+                        <option value="sortie d'ecole"> sortie d'ecole</option>
+                        <option value="autre"> autre</option>
+                    </select>
+                </p>
+                
+                
+                
+                <div>
+					<label for="datenaissance">
+						Date de naissance:
+					</label>
+					<input type="date" class="input-medium search-query" name="datenaissance" placeholder= "jour/mois/année" />
+				</div>
+				
+                <div>
+					<label for="MDP">
+						Mot de passe:
+					</label>
+					<input type="password" class="input-medium search-query" name="MPD" placeholder= "Votre mot de passe" />
+				</div>
+                
+                 <div>
+					<label for="MDPconfirmation">
+						Confirmation de mot de passe:
+					</label>
+					<input type="password" class="input-medium search-query" name="MDPconfirmation" placeholder= "Confirmer votre mot de passe" />
+				</div>              
+                <div >
+				    <label>
+					 <a href='profil.php'><button type="submit" class="btn" name="inscription">Inscription</button></a>
+					 <a href='inscription.php'><button type="reset" class="btn">Effacer</button></a>
+                    </label>
+                </div>
+                <div>
+					<label for="bla">
+						C'est fini 
+					</label> 
+				</div>
+			</form>
+            
+</section>             
+    
+ <?php 
+
+    include('footer.php'); 
+?>
---
+++ Projet-Craquette-master/traitement.php
@@ -0,0 +1,100 @@
+<?php
+    try{ 
+        $bdd = new PDO('mysql:host=localhost;dbname=siteweb;charset=utf8','root',''); // stocker la connexion à la base de données dans la variable $bdd
+    }
+    catch(Exception $e){ // si cela ne fonctionne pas : attraper l'erreur...
+        die('Erreur : '.$e->getMessage()); // ... arrêter le processus et afficher l'erreur
+    }
+?>
+
+
+<?php
+	$id = $bdd ->query('SELECT max(Id) FROM profil ') + 1;// recupérer id suivant de base d
+    //echo($id);
+//$id = $reponse->fetch();
+     //$reponse2 = $bdd->query('SELECT * FROM profil ORDER BY id DESC'); 
+?>
+    
+
+<!doctype html>
+<html lang="fr">
+
+	<head> <!-- en tête du fichier -->
+		<meta charset="utf-8"/>
+		<title>Craquette - Connexion</title>
+		<link rel="stylesheet" href="bootstrap.css"/>
+	</head>
+	<body>
+		<header> <!-- header = en tête page -->
+            <p>
+                <img src="Images/logo.jpg" alt="logo" title="Logo de Craquette" height=100px width=100px/>
+            </p>
+            <p>Craquette</p>
+		</header>
+		
+		<section id="contenu"> <!-- Contenu principal de la page -->
+
+
+
+<?php
+
+if(isset($_POST['inscription'])){ // si le bouton envoi a été cliqué
+	if(!empty($_POST['nom'])){ // si le champ nom a été rempli
+		$nom = $_POST['nom']; // stocker la valeur qu'il contient dans une variable
+		
+		if(!empty($_POST['prenom'])){ 
+			$prenom = $_POST['prenom'];     
+				
+			if(!empty($_POST['mail'])){ 
+				$mail = $_POST['mail'];
+				
+				if(!empty($_POST['datenaissance'])){ 
+					$date = $_POST['datenaissance'];
+					
+					if(!empty($_POST['promo'])){ 
+						$promo = $_POST['promo']; 
+						
+						if(!empty($_POST['filiere'])){ 
+							$filiere = $_POST['filiere']; 
+							echo("azed");
+                            
+						 	if(!empty($_POST['MDP'])) {
+                                echo("coucou");
+                                if($_POST['MDP'] == $_POST['MDPconfirmation']){ 
+                                    $motDePass=$_POST['MDP'];
+                                    $id = $id + 1 ;
+                                    $option = [
+                                        $nom -> $id,
+                                        $prenom -> $id + 5,
+                                    ];
+                                    $sel = password_hash($motDePass,PASSWORD_DEFAULT, $option);
+                                    echo("coucou");
+                                    $insertion = $bdd->prepare('INSERT INTO profil VALUES("'.$id.'","'.$nom.'","'.$prenom.'","'.$sel.'","'.$mail.'","NULL","'.$date.'","'.$promo.'","'.$filiere.'","NULL")'); // préparation de la requête d'insertion dans la base de données
+                                    $insertion->execute();  // exécution de l'insertion
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+     
+
+?>
+            <p> 
+            <br>
+                Votre profil a bien été créé.
+            
+            </p>
+ <a href='profil.php'><button type="submit" class="btn">Voir mon profil</button></a>
+         <p><br></p> 
+<?php
+             echo '<p>'.$prenom.' '.$nom.', merci de nous rejoindre .</p>';          
+            /* echo '<p>'.$promo.' '.$adresse.'</p>'; 
+            echo'<p>'.$date.'</p>';*/
+ // include('footer.php');          
+            
+?>
+
