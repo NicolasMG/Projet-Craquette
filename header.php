63fedc506c51c4200cbe731fb175d06d47fd2b5d
rachelnoireau <rachel.noireau@uha.fr>
2017-06-01 12:10:30 +0200

    photo

--- header.php
+++ header.php
@@ -5,13 +5,49 @@
 		<meta charset="utf-8"/>
 		<title>Craquette - Connexion</title>
 		<link rel="stylesheet" href="CSS/bootstrap.css"/>
+        <script type="text/javascript" src="calendrier.js"></script>
 	</head>
 	<body>
 		<header> <!-- header = en tête page -->
-            <p>
-                <a href='accueil.php'><img src="Images/logo.jpg" alt="logo" title="Logo de Craquette" height=100px width=100px/></a>
-            </p>
-            <p>Craquette</p>
+             <ul  id="menu_horizontal">
+                <li class="bouton_gauche">
+                    <img src="Images/logo.png" alt="Photo de montagne" width="100" height="100"/>
+                    <p>Craquette</p>
+                </li>
+	
+                <li class="bouton_gauche1">
+                    <form>
+                        <input type="text" name="Search" placeholder="Recherche..">
+                    </form>
+                </li>
+	
+
+                <li class="bouton_droite">
+
+                        <a href="header.html" ><img src="roue.png" alt="Photo de montagne" width="35"  /></a>
+
+                </li>
+
+
+                <li class="bouton_droite">
+
+                    <img src="ami.png" alt="Photo de montagne" width="35"  />
+
+                </li>
+
+                <li class="bouton_droite">
+
+                        <img src="enveloppe.png" alt="Photo de montagne" width="35"  />
+
+                </li>
+
+                <li class="bouton_droite">
+
+                        <img src="alarme.png" alt="Photo de montagne" width="35"   />
+
+
+                </li>
+            </ul>
 		</header>
 		
 		
