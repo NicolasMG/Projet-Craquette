2fcef32db808481846ba751dc953ff088ecbdf07
Alypha <david.vergnault@hotmail.fr>
2017-06-01 14:11:48 +0200

    David Modif Accueil 01/06 14h

---
+++ accueil.php
@@ -0,0 +1,104 @@
+<?php include('header.php'); ?>
+
+<div class="inlineblock">
+   
+	
+	
+	
+	
+
+    <div id="vide_gauche"></div>
+    <div id="contenu_gauche">    
+        <div id="profil">
+            <a href='profil.php'><img style="display:block; position:absolute; left:205px; top:45px; width:280px; height:103px;" src="Images/couverture.jpg" alt="couverture" title="couverture" height=50px width=50px/></a>
+             <a href='profil.php'><img class="img-circle" style="display:block; position:absolute; left:225px; top:80px; width:100px; height:100px;" src="Images/photo_profil.jpg" alt="photo_profil" title="photo_profil" height=100px width=100px/></a>
+            <p style="display:block; position:absolute; left:235px; top:70px;font-weight: bold; color:white;">Jean-Michel LeSaumon</p>
+            <button type="submit" class="btn" style="display:block; position:absolute; left:360px; top:155px;">Se déconnecter</button>
+        </div>
+        <div id="groupes">
+            <ul>Groupes
+                <li>Groupe 1</li>
+                <li>Groupe 2</li>
+                <li>Groupe 3</li>  
+            </ul>
+            <ul>Pages    
+                <li>Pages 1</li>
+                <li>Pages 2 avec un nom long</li>
+                <li>Pages 3</li>
+            </ul>
+             <ul>Evenements    
+                <li>Evenement 1</li>
+                <li>Evenement 2</li>
+                <li>Evenement 3</li>
+            </ul>
+        </div>
+    </div>
+    <div id="contenu_centre">
+        <div id="post">
+            <div class="textemessage">
+                Poster un message :
+                <textarea name="message" rows="3" cols="200"></textarea>
+                <button type="submit" class="btn">Publier</button>        
+            </div>
+        </div>
+        <div id="filactualite">
+                  
+        </div>
+    </div>
+
+
+    <div id="contenu_droite">
+        <div id="amis">
+            <p>Ami 1</p>
+            <p>Ami 2</p>
+            <p>Ami 3</p>
+            <p>Ami 4</p>
+            <p>Ami 5</p>
+            <p>Ami 6</p>
+            <p>Ami 7</p>
+            <p>Ami 8</p>
+            <p>Ami 9</p>
+            <p>Ami 9</p>
+            <p>Ami 9</p>
+            <p>Ami 9</p>
+            <p>Ami 9</p>
+            <p>Ami 9</p>
+            <p>Ami 9</p>
+            <p>Ami 9</p>
+            <p>Ami 9</p>
+            <p>Ami 9</p>
+            <p>Ami 9</p>
+            <p>Ami 9</p>
+            <p>Ami 9</p>
+            <p>Ami 9</p>
+            <p>Ami 9</p>
+            <p>Ami 9</p>
+            <p>Ami 9</p>
+            <p>Ami 9</p>
+            <p>Ami 9</p>
+            <p>Ami 9</p>
+            <p>Ami 9</p>
+            <p>...</p>
+        </div>
+    </div>
+    <div id="calendrier">
+        <div class="calendrier">
+            <script type="text/javascript">
+                    calendrier();
+            </script>
+        </div>
+        <div>
+       <p><a href="https://www.e-services.uha.fr/">E-service UHA</a></p> 
+       <p><a href="http://www.ensisa.uha.fr/">ENSISA</a></p> 
+        
+        </div>
+    </div>
+     
+    
+</div>
+
+    
+
+
+
+
