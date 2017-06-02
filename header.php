46ce5185d4a0080d76506eec9fa6472fe9f18888
Alypha <david.vergnault@hotmail.fr>
2017-06-02 10:00:34 +0200

    Add files via upload

---
+++ header.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<?php
+    //pour savoir a qui appartient la session et recuperer des donné
+   // session_start();
+   // $mail = $_SESSION['ID'];
+    try{ 
+        $bdd = new PDO('mysql:host=localhost;dbname=siteweb;charset=utf8','root',''); 
+    }
+    catch(Exception $e){ 
+        die('Erreur : '.$e->getMessage()); 
+    }
+
+
+?>
+
+<html lang="fr">
+
+	<head> <!-- en tête du fichier -->
+		<meta charset="utf-8"/>
+		<title>Craquette - Connexion</title>
+		<link rel="stylesheet" href="CSS/bootstrap.css"/>
+        <script type="text/javascript" src="calendrier.js"></script>
+	</head>
+	<body>
+		<header> <!-- header = en tête page -->
+
+          <div style="height:50px;" class="topnav" id="myTopnav">
+              <img style="padding-left:320px;" class="headerimg" src='Images/logo.png' />
+              
+		      
+</div>
+
+
+
+		</header>
+		
+		
