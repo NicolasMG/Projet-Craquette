72ce9f987052eabc7f26693e3728f4520b54c1a8
Alypha <david.vergnault@hotmail.fr>
2017-06-06 12:07:06 +0200

    REMISE EN FORME GITHUB

---
+++ PHP/chgtmdp.php
@@ -0,0 +1,42 @@
+<?php include ('header.php'); 
+    try{ 
+        $bdd = new PDO('mysql:host=localhost;dbname=siteweb;charset=utf8','root',''); // stocker la connexion à la base de données dans la variable $bdd
+    }
+    catch(Exception $e){ // si cela ne fonctionne pas : attraper l'erreur...
+        die('Erreur : '.$e->getMessage()); // ... arrêter le processus et afficher l'erreur
+    }
+        
+    session_start();
+    $mail=$_SESSION['mail'];
+
+?>
+
+<form class="form-horizontal" method="post" action="traitementmodif.php" enctype="multipart/form-data">
+          <form class="form-horizontal" method="post" action="traitement.php" >
+                
+                 <div>
+					<label for="mail">
+						Adresse Email : 
+					</label>
+					<input type="mail" class="input-medium search-query" name="mail" placeholder= "Email" />  
+				</div>
+              
+              <div>
+					<label for="nouveaumdp">
+						Nouveau mot de passe:
+					</label>
+					<input type="password" class="input-medium search-query" name="nouveaumdp" placeholder= "Mot de passe" />  
+				</div>
+                
+                <div>
+					<label for="confirmermdp">
+						Confirmer nouveau mot de passe:
+					</label>
+					<input type="password" class="input-medium search-query" name="confirmermdp" placeholder= "Mot de passe" />
+                </div>
+            <a href='chgtmdpreussie'><button type="submit" class="btn" name="chgtmdpreussie">Confirmer</button></a>
+              <br>
+
+    </form>
+
+<?php include ('footer.php'); ?>
--- PHP/header_profil.php
+++ PHP/header_profil.php
@@ -14,7 +14,7 @@
 		<title>Craquette - Connexion</title>
 		<link rel="stylesheet" href="CSS/bootstrap.css"/>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        <script type="text/javascript" src="calendrier.js"></script>
+        <script type="text/javascript" src="JS/calendrier.js"></script>
 	</head>
 	<body id="body_profil">
 		<header> <!-- header = en tête page -->
