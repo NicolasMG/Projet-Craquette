634f1cfd0eb35fece265fb77ca1dc45366c3e17e
Alypha <david.vergnault@hotmail.fr>
2017-06-07 11:02:11 +0200

    David edt 11h
    OFFICIELEMENT IMPOSSIBLE DE RECUP LEMPOI DU TEMPS

--- PHP/emploidutemps.php
+++
@@ -1,5 +0,0 @@
-<?php include ('header.php') ?>
-
-<img style="margin-top:3%;" src='Images/Emloi_du_temps/29-05-2017.png'/>
-<a href='profil.php'><button style="left:49%; top:85%;" type="submit" class="btn">Retour au profil</button></a>
-<?php include ('footer.php') ?>
