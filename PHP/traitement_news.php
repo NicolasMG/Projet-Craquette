659769212971ad0196f7fcb40e6aae4f5cbf81d1
NicolasMG <nicolas.m.greiner@gmail.com>
2017-06-07 09:11:06 +0200

    Correction du traitement new message empty
    Nicolas
    9h10
    Correction dans le cas ou les messages sont vides, désormais, ils seront passés

--- PHP/traitement_news.php
+++ PHP/traitement_news.php
@@ -2,15 +2,15 @@
 if (isset($_POST['craquetter'])){
 	if(!empty($_POST['message'])){
 		$message=htmlspecialchars($_POST['message']);
+        try{
+            $bdd = new PDO('mysql:host=localhost;dbname=profil_newsfeed;charset=utf8','root','',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        }
+        catch (Exception $e){
+            die('connection failed : '.$e->getMessage());
+        }
+        $req=$bdd->prepare('insert into actualite (poste) values ("'.$message.'")');
+        $req->execute();
 	}
 }
-try{
-	$bdd = new PDO('mysql:host=localhost;dbname=profil_newsfeed;charset=utf8','root','',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-}
-catch (Exception $e){
-	die('connection failed : '.$e->getMessage());
-}
-$req=$bdd->prepare('insert into actualite (poste) values ("'.$message.'")');
-$req->execute();
 header('Location: ./accueil.php');
 ?>
