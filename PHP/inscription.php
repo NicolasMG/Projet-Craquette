c225510f1ee837edbd0e7aed01d7c28bdf55accc
nicolas greiner <greiner@uha.fr>
2017-06-09 16:36:40 +0200

    indentation + correction + mot de passe > 5 caracteres
    16h36
    Nicolas

--- PHP/inscription.php
+++ PHP/inscription.php
@@ -17,14 +17,7 @@
             <h2>Inscription</h2>
                 
             <form class="form-horizontal" method="post" action="traitement.php" >
-                
-             <div class="form-group">
-                    <label for="MDP">Mot de passe :</label>
-                    <div style="display:inline;">
-                        <input class="form-control" style="width:250px; display:inline;" type="password" name="MDP" placeholder="Mot de passe" />
-                    </div>
-                </div>
-                
+
                 
                 
                 <div class="form-group">
--- PHP/traitement.php
+++ PHP/traitement.php
@@ -14,12 +14,8 @@
         $sql=$bdd->prepare('SELECT max(Id) FROM profil ');
         $sql->execute();
         $id = $sql->fetch();
-
-
 ?>
     
-
-
 <?php
 $valideinscription="erreurinscription.php";
 $message="Oups....une erreur c'est glisser dans votre formulaire d'inscription";
@@ -46,66 +42,66 @@ if(isset($_POST['inscription'])){ // si le bouton envoi a été cliqué
 						if(!empty($_POST['filiere'])){
 							$filiere = htmlspecialchars($_POST['filiere']);
                             
-                                //echo 'hello';
                             
                             $MDP=htmlspecialchars($_POST['MDP']);
                             $MDPC=htmlspecialchars($_POST['MDPconfirmation']);
                             
 				            if(!empty($MDP) && !empty($MDPC)) {
                                 
-                                //echo ' ici ';
-                                
-                                if($MDP == $MDPC){ 
-                                    $ide = $id[0] ; //id est auto incrementé se serai mieux
-                                    $option = [
-                                        $nom => $ide + 1,
-                                        $prenom => $ide + 6,
-                                    ];
-                                    echo "<br>";
-                                    //echo $MDP;
-                                    $sel = password_hash($MDP,PASSWORD_DEFAULT, $option);
-                                                                 
+                                if(strlen($MDP)>5){ // taille du mot de passe
 
-                                    $reponse=$bdd->prepare('Select email From profil Where email="'.$mail.'"');
-                                    $mail=htmlentities($_POST['mail']);
-                                    $reponse->execute(array('.$mail.'=>htmlspecialchars($_POST['mail'])));
-                                    $reponse2=$reponse->fetch();
-                                    
-                                    if(!$reponse2){
-                                        
-                                      /*  echo "ca marche";
+                                    if($MDP == $MDPC){ 
+                                        $ide = $id[0] ; //id est auto incrementé se serai mieux
+                                        $option = [
+                                            $nom => $ide + 1,
+                                            $prenom => $ide + 6,
+                                        ];
                                         echo "<br>";
-                                        echo $ide +1;
-                                        echo "<br>";
-                                        echo $nom;
-                                        echo "<br>";
-                                        echo $prenom;
-                                        echo "<br>";
-                                        echo $sel;
-                                        echo "<br>";
-                                        echo $mail;
-                                        echo "<br>";
-                                        echo $date;
-                                        echo "<br>";
-                                        echo $promo;
-                                        echo "<br>";
-                                        echo $filiere;
-                                        echo "<br>";*/
-                                        
-                                        $idef = $ide +1 ;
-                                        $insertion = $bdd->prepare('INSERT INTO profil VALUES("'.$idef.'","'.$nom.'","'.$prenom.'","'.$sel.'","'.$mail.'","'.$date.'","'.$promo.'","'.$filiere.'","NULL","NULL","Images/profilpardefaut.png","Images/couverturepardefaut.jpg","NULL")'); // préparation de la requête d'insertion dans la base de données
-                                        $insertion->execute();  // exécution de l'insertion
-                                        
-                                        
-                                        session_start();
-                                        $_SESSION['mail']= $mail;
-                                        $_SESSION['ID']=$idef;
-                                        $_SESSION['MDPS']= $sel;
-                                       
-                                        $message="Votre profil a bien été créé.";
-                                        $bouton="Voir mon profil";
-                                        $valideinscription="profil.php";
-                                        $message2= "merci de nous rejoindre";
+                                        //echo $MDP;
+                                        $sel = password_hash($MDP,PASSWORD_DEFAULT, $option);
+
+
+                                        $reponse=$bdd->prepare('Select email From profil Where email="'.$mail.'"');
+                                        $mail=htmlentities($_POST['mail']);
+                                        $reponse->execute(array('.$mail.'=>htmlspecialchars($_POST['mail'])));
+                                        $reponse2=$reponse->fetch();
+
+                                        if(!$reponse2){
+
+                                          /*  echo "ca marche";
+                                            echo "<br>";
+                                            echo $ide +1;
+                                            echo "<br>";
+                                            echo $nom;
+                                            echo "<br>";
+                                            echo $prenom;
+                                            echo "<br>";
+                                            echo $sel;
+                                            echo "<br>";
+                                            echo $mail;
+                                            echo "<br>";
+                                            echo $date;
+                                            echo "<br>";
+                                            echo $promo;
+                                            echo "<br>";
+                                            echo $filiere;
+                                            echo "<br>";*/
+
+                                            $idef = $ide +1 ;
+                                            $insertion = $bdd->prepare('INSERT INTO profil VALUES("'.$idef.'","'.$nom.'","'.$prenom.'","'.$sel.'","'.$mail.'","'.$date.'","'.$promo.'","'.$filiere.'","NULL","NULL","Images/profilpardefaut.png","Images/couverturepardefaut.jpg","NULL")'); // préparation de la requête d'insertion dans la base de données
+                                            $insertion->execute();  // exécution de l'insertion
+
+
+                                            session_start();
+                                            $_SESSION['mail']= $mail;
+                                            $_SESSION['ID']=$idef;
+                                            $_SESSION['MDPS']= $sel;
+
+                                            $message="Votre profil a bien été créé.";
+                                            $bouton="Voir mon profil";
+                                            $valideinscription="profil.php";
+                                            $message2= "merci de nous rejoindre";
+                                        }
                                     }
                                 }
                             }
