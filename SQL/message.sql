412ce32a01b0255d61fdad4e607aa116abda05b2
rachelnoireau <rachel.noireau@uha.fr>
2017-06-09 15:42:30 +0200

    rachel

--- SQL/message.sql
+++ SQL/message.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Jeu 08 Juin 2017 à 20:38
+-- Généré le :  Ven 09 Juin 2017 à 15:29
 -- Version du serveur :  5.7.11
 -- Version de PHP :  5.6.19
 
@@ -30,31 +30,25 @@ CREATE TABLE `message` (
   `id` int(11) NOT NULL,
   `idutil1` int(11) NOT NULL,
   `idutil2` int(11) NOT NULL,
-  `message` text COLLATE utf8_unicode_ci NOT NULL
+  `mp` text COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Contenu de la table `message`
 --
 
-INSERT INTO `message` (`id`, `idutil1`, `idutil2`, `message`) VALUES
-(1, 26, 27, 'salut'),
-(2, 26, 27, 'rachou'),
-(3, 26, 27, 'yoyo'),
-(4, 26, 27, 'coucou'),
-(6, 26, 27, 'yo'),
-(7, 26, 27, 'yo'),
-(8, 26, 27, 'yo'),
-(9, 26, 27, 'F.F.'),
-(10, 26, 27, 'chat'),
-(11, 26, 27, 'le p\'ti gras'),
-(12, 26, 27, 'et gros et plus'),
-(13, 26, 27, '*en'),
-(14, 26, 27, 'salutsalut'),
-(15, 27, 26, 't\'es moche'),
-(16, 27, 26, 'tu me fais peur'),
-(17, 27, 26, 'Faut que tu arrêtes les cris chelou comme ça ... !!!!\r\n'),
-(18, 27, 26, 'aie');
+INSERT INTO `message` (`id`, `idutil1`, `idutil2`, `mp`) VALUES
+(1, 0, 0, 'salut\r\n'),
+(2, 0, 0, 't\'es moche'),
+(3, 0, 0, 'kikou'),
+(4, 0, 0, 'vous etes tous moches'),
+(5, 26, 27, 'salut\r\n'),
+(6, 26, 27, 'salut\r\n'),
+(7, 26, 27, 'salut\r\n'),
+(8, 26, 27, 'de rachel a rn'),
+(9, 27, 27, 'coucou'),
+(10, 27, 26, 'de rn a rachel'),
+(11, 27, 26, 't\'es moche');
 
 --
 -- Index pour les tables exportées
@@ -74,7 +68,7 @@ ALTER TABLE `message`
 -- AUTO_INCREMENT pour la table `message`
 --
 ALTER TABLE `message`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
